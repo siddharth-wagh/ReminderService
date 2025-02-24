8d4d55860effb1b5d49f7443fd98ca46b4e95313
sid <siddhuwagh27112004@gmail.com>
2025-02-24 23:38:38 +0530

    Created notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser =require('body-parser');
 const { PORT } = require('./config/serverconfig');
 const {sendBasicEmail} = require('./services/emailServices');
-
+const cron = require('node-cron');
 
 
 
@@ -23,6 +23,12 @@ const setupAndStartServer = () => {
         //     "Testing the working of email sending",
         //     "Hey, This email is send to ensure that the nodemailsender is working properly"
         // )
+
+       
+
+        cron.schedule('*/2 * * * * *', () => {
+            console.log('running a task every minute');
+        });
     })
 }
 
